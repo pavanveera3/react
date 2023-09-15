6f89b3a2dc6b4161bcf2caa68cfa3fd5eb486774
root <root@ip-172-31-4-35.ca-central-1.compute.internal>
2023-09-15 08:41:17 +0000

    Testing 123

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Amplifyxx  
+          Learn Amplifyxaaa  
         </a>
       </header>
     </div>

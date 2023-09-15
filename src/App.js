80115afe558cb09aba7ad03eb351bb9d533170da
root <root@ip-172-31-4-35.ca-central-1.compute.internal>
2023-09-15 08:02:52 +0000

    Testing

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Veera  
+          Learn Amplifyxx  
         </a>
       </header>
     </div>

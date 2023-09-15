92ecbe0b848936bac803288c46f01f3b1af485b0
root <root@ip-172-31-4-35.ca-central-1.compute.internal>
2023-09-15 09:00:09 +0000

    Happy Ramya

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Amplifyxaaaaaaaaaa  
+          Learn AmplifyxaaaaaaaaaaRamya  
         </a>
       </header>
     </div>
